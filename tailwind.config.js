f6f787576295e10eb840b9bc9acd0b71371c9a7d
Ma'ruf Shabonov <shabonov.m@bnpz.local>
2024-11-29 12:40:49 +0500

    cha

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,6 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'back': "url('./back.png')",
-       
-      }
-    },
-  },
   plugins: [],
 }
 
